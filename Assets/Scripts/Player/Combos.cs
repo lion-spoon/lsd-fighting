42e259cc4d2572426c170f0c68f338d503724dcf
Luis Henrique <luiscostasa@hotmail.com>
2022-06-06 15:17:50 -0300

    Ninja animation

--- Assets/Scripts/Player/Combos.cs
+++ Assets/Scripts/Player/Combos.cs
@@ -8,6 +8,9 @@ public class Combos : MonoBehaviour
     public int noOfClicks = 0;
     float lastClickedTime = 0;
     public float maxComboDelay = 0.9f;
+    public int noOfClicksLight = 0;
+    float lastClickedTimeLight = 0;
+    public float maxComboDelayLight = 0.9f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,41 +23,104 @@ public class Combos : MonoBehaviour
         if(Time.time - lastClickedTime > maxComboDelay){
             noOfClicks = 0;
         }
-        if(Input.GetMouseButtonDown(0)){
+        if(Input.GetMouseButtonDown(1)){
             lastClickedTime = Time.time;
             noOfClicks++;
             if(noOfClicks == 1){
-                animator.SetBool("WeakPunch", true);
+                animator.SetBool("heavy1", true);
+            }else{
+                animator.SetBool("heavy1", false);
             }
-            noOfClicks = Mathf.Clamp(noOfClicks, 0, 3);
+            noOfClicks = Mathf.Clamp(noOfClicks, 0, 4);
         }
+        if(Time.time - lastClickedTimeLight > maxComboDelayLight){
+            noOfClicksLight = 0;
+        }
+        if(Input.GetMouseButtonDown(0)){
+            lastClickedTimeLight = Time.time;
+            noOfClicksLight++;
+            if(noOfClicksLight == 1){
+                animator.SetBool("light1", true);
+            }else{
+                animator.SetBool("light1", false);
+            }
+            noOfClicksLight = Mathf.Clamp(noOfClicksLight, 0, 4);
+        }
+    }
+
+    public void returnLight1(){
+        if(noOfClicksLight >=2){
+            animator.SetBool("light2", true);
+        }else{
+            animator.SetBool("light1", false);
+            noOfClicksLight = 0;
+        }
+    }
+
+    public void returnLight2(){
+        if(noOfClicksLight >= 3){
+            animator.SetBool("light3", true);
+        }else{
+            animator.SetBool("light1", false);
+            animator.SetBool("light2", false);
+            noOfClicksLight = 0;
+        }	
+    }
+
+    public void returnLight3(){
+        if(noOfClicksLight >=4){
+            animator.SetBool("light4", true);
+        }else{
+            animator.SetBool("light1", false);
+            animator.SetBool("light2", false);
+            animator.SetBool("light3", false);
+            noOfClicksLight = 0;
+        }	
+    }
+
+    public void returnLight4(){
+        animator.SetBool("light1", false);
+        animator.SetBool("light2", false);
+        animator.SetBool("light3", false);
+        animator.SetBool("light4", false);
+        noOfClicksLight = 0;
     }
 
     public void return1(){
         if(noOfClicks >= 2){
-            animator.SetBool("HeavyPunch", true);
+            animator.SetBool("heavy2", true);
         }else{
-            animator.SetBool("WeakPunch", false);
+            animator.SetBool("heavy1", false);
             noOfClicks = 0;
         }
     }
 
     public void return2(){
         if(noOfClicks >= 3){
-            animator.SetBool("Trip", true);
+            animator.SetBool("heavy3", true);
         }else{
-            animator.SetBool("WeakPunch", false);
-            animator.SetBool("HeavyPunch", false);
+            animator.SetBool("heavy1", false);
+            animator.SetBool("heavy2", false);
             noOfClicks = 0;
         }
     }
 
     public void return3(){
+        if(noOfClicks >= 4){
+            animator.SetBool("heavy4", true);
+        }else{
+            animator.SetBool("heavy1", false);
+            animator.SetBool("heavy2", false);
+            animator.SetBool("heavy3", false);
+            noOfClicks = 0;
+        }
+    }
 
-        animator.SetBool("WeakPunch", false);
-        animator.SetBool("HeavyPunch", false);
-        animator.SetBool("Trip", false);
+    public void return4(){
+        animator.SetBool("heavy1", false);
+        animator.SetBool("heavy2", false);
+        animator.SetBool("heavy3", false);
+        animator.SetBool("heavy4", false);
         noOfClicks = 0;
-
     }
 }
